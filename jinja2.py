07962a1f8850d75960ac93e57d661170a1d2c6da
revillnik <revillnik@gmail.com>
2024-01-11 10:30:28 +0300

    next

---
+++ jinja2.py
@@ -0,0 +1,11 @@
+import jinja2
+
+class Person:
+	def __init__(self, age, name):
+		self.age = age
+		self.name = name
+per = Person(25, 'Nikita')
+x = Template('Меня зовут {{p.name}}, возраст: {{p.age}}')
+y = x.render(p = per)
+print(y)
+
