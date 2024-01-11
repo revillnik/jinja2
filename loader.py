c858adf40c89f99ea5a7e8cc39b3830b5c9ec0a5
revillnik <revillnik@gmail.com>
2024-01-11 21:29:57 +0300

    next

---
+++ loader.py
@@ -0,0 +1,19 @@
+from jinja2 import Template, FunctionLoader, Environment
+
+pupil = {"name": "Никита", "age": 20}
+
+
+def load(x):
+    if x == "1":
+        return """Меня зовут {{c.name}}, мне {{c.age}}"""
+    else:
+        return """Такого человека тут нет"""
+
+
+lod = FunctionLoader(load)
+env = Environment(loader=lod)
+
+tm = env.get_template('1')
+f = tm.render(c=pupil)
+print(f)
+
